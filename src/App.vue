500226da1d1218fe86557d5d466e04a4767cbffb
Victor Palade <victor@cloudflavor.io>
2019-11-15 00:18:21 +0100

    layout: add containers for cards.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <template>
   <v-app>
-    <router-view> </router-view>
+    <v-content>
+      <v-container>
+        <v-card align="center">
+          <v-card-title center>Memory cards</v-card-title>
+          <v-row class="d-flex mb-6 ">
+            <span> {{ item }} </span>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-card
+                elevation="8"
+                class="d-flex justify-center shaped"
+                height="200"
+                width="300px"
+                justify="center"
+                padding="left"
+                ><span>Test</span></v-card
+              >
+            </v-col>
+            <v-col>
+              <v-card
+                elevation="8"
+                class="d-flex justify-center"
+                height="200px"
+                width="300px"
+                ><span>Test</span></v-card
+              >
+            </v-col>
+            <v-col>
+              <v-card
+                elevation="8"
+                class="d-flex justify-center"
+                height="200px"
+                width="300px"
+                ><span>Test</span></v-card
+              >
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "memory-cards"
+  name: "memory-cards",
+  data: () => ({
+    item: "Hund"
+  })
 };
 </script>
